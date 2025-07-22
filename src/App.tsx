d7caba50b11a371a6c323327e25fda9dbf4fe2d1
GR0790 <benzman0790@gmail.com>
2025-07-22 15:03:13 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Contact from "./pages/Contact";
 import CreditCardService from "./pages/CreditCardService";
 import FAQ from "./pages/FAQ";
 import GiftCardService from "./pages/GiftCardService";
-// import Home from "./pages/Home"; // Home 컴포넌트를 잠시 비활성화합니다.
+import Home from "./pages/Home";
 import HowToUse from "./pages/HowToUse";
 import InformationFeeService from "./pages/InformationFeeService";
 import MicropaymentService from "./pages/MicropaymentService";
@@ -20,18 +20,7 @@ function App() {
   return (
     <Layout>
       <Routes>
-        {/* 첫 페이지에서 Home 컴포넌트 대신 간단한 환영 메시지를 표시합니다. */}
-        <Route
-          path="/"
-          element={
-            <div className="container mx-auto p-8 text-center">
-              <h1 className="text-4xl font-bold">테스트 페이지</h1>
-              <p className="mt-4 text-lg">
-                이 메시지가 보이면, 문제는 Home.tsx 파일에 있습니다.
-              </p>
-            </div>
-          }
-        />
+        <Route path="/" element={<Home />} />
         <Route
           path="/services/micropayment"
           element={<MicropaymentService />}
