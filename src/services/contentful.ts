e96983d2076da9187b2452a1d54bdc13167b87ec
GR0790 <benzman0790@gmail.com>
2025-07-29 20:18:40 +0900

    Update contentful.ts

--- src/services/contentful.ts
+++ src/services/contentful.ts
@@ -1,6 +1,5 @@
 import { createClient } from 'contentful';
 
-// .env.local 파일에 저장된 API 키를 안전하게 불러옵니다.
 const client = createClient({
   space: import.meta.env.VITE_CONTENTFUL_SPACE_ID as string,
   accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN as string,
